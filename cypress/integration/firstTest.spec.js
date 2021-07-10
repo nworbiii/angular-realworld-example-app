fcf289dc5b47e26d7808affab37ca9f9648533cf
Kyle Brown <kylealbrown@gmail.com>
2021-07-10 17:58:09 -0400

    adds env variables

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -92,7 +92,7 @@ describe('Test with backend', () => {
         cy.get('@token').then((token) => {
 
             cy.request({
-                url: 'https://conduit.productionready.io/api/articles/',
+                url: Cypress.env('apiUrl') + 'api/articles/',
                 headers: { 'Authorization': 'Token ' + token },
                 method: 'POST',
                 body: bodyRequest
@@ -105,7 +105,7 @@ describe('Test with backend', () => {
             cy.get('.article-actions').contains('Delete Article').click()
 
             cy.request({
-                url: 'https://conduit.productionready.io/api/articles?limit=10&offset=0',
+                url: Cypress.env('apiUrl') + 'api/articles?limit=10&offset=0',
                 headers: { 'Authorization': 'Token ' + token },
                 method: 'GET'
             }).its('body').then((body) => {
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,20 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  if (!fs.existsSync(pathToConfigFile)) {
+    return {}
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  const file = config.env.configFile
+
+  return getConfigurationByFile(file)
 }
